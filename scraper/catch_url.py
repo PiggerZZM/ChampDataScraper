ec142e217af229f1087cf1b72877e0329afb8bb7
zx <2762233248@qq.com>
2021-07-23 17:42:58 +0800

    change file name

--- scraper/catch_url.py
+++ scraper/catch_url.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 base_url = "http://data.champdas.com"
 
-def catch_url_year(url):
+def catch_url_year(url,num):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
     spans = soup.find_all(class_='matchNote')
@@ -14,13 +15,13 @@ def catch_url_year(url):
             a = span.find(name='a')
             if a:
                 href = a.get('href')
-                file = open('zc_url.txt', 'a', encoding='utf-8')
+                file = open('url.txt', 'a', encoding='utf-8')
                 file.write(base_url + href + '\n')
                 file.close()
 
 
 def catch_url_two(num):
-    file = open('zc_url.txt', 'a', encoding='utf-8')
+    file = open('url.txt', 'a', encoding='utf-8')
     if num == 1:
         file.write("\nzc\n")
     else:
@@ -36,7 +37,5 @@ def catch_url_two(num):
 
 if __name__ == '__main__':
     catch_url_two(1)
-
-    catch_url_two(2)
 
 
