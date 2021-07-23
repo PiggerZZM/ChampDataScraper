644adee5c5c3e641f0464149a25c2aa4c582cd44
zx <2762233248@qq.com>
2021-07-23 17:48:07 +0800

    finish debug

--- scraper/catch_url.py
+++ scraper/catch_url.py
@@ -4,7 +4,7 @@ import csv
 
 base_url = "http://data.champdas.com"
 
-def catch_url_year(url,num):
+def catch_url_year(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
     spans = soup.find_all(class_='matchNote')
@@ -22,10 +22,6 @@ def catch_url_year(url,num):
 
 def catch_url_two(num):
     file = open('url.txt', 'a', encoding='utf-8')
-    if num == 1:
-        file.write("\nzc\n")
-    else:
-        file.write("\nzj\n")
     file.close()
     for year in range(2016, 2022):
         for round in range(1, 31):
@@ -37,5 +33,7 @@ def catch_url_two(num):
 
 if __name__ == '__main__':
     catch_url_two(1)
+    #catch_url_two(2)
+
 
 
