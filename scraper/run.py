581d2c1721061e86bb2ade12d80cd27142327804
PiggerZZM <442823793@qq.com>
2021-07-26 18:30:38 +0800

    add run.py

--- scraper/run.py
+++ scraper/run.py
@@ -1,27 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
-import lxml
 import logging
+from typing import List
+
+from scraper.get_all_match_urls import get_all_match_urls
+from scraper.get_json_data import get_match_id, get_json_data
 
 # 日志设置
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+from scraper.get_team_names import get_team_names
+from scraper.json_to_csv import json_to_csv
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+
+
+def run(years: List[str]):
+    # 获取所有中超中甲比赛详情页url
+    match_urls = get_all_match_urls(years)
+
+    ajax_url = "http://data.champdas.com/getMatchStaticListAjax.html"
+
+    for match_url in match_urls:
+        # 获取比赛球队名称
+        l_team, r_team = get_team_names(match_url)
+        # 获取比赛id
+        match_id = get_match_id(match_url)
+        json_data_list = []
+        for half in [0, 1, 2]:
+            # 请求"球队数据"
+            json_data_list.append(get_json_data(ajax_url, match_id, half, l_team, r_team))
+        # 存储
+        json_to_csv(json_data_list, file_name=r"match_data.csv")
+
 
 if __name__ == '__main__':
-    # 请求
-    url = "http://data.champdas.com/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        print(response.status_code)
-        logging.debug("请求成功")
-
-    # 解析
-    html = response.text
-    soup = BeautifulSoup(html, 'lxml')
-    if soup is not None:
-        logging.debug("解析成功")
-
-    # 保存
-    with open(r'../data/result.html', 'w', encoding='utf-8') as file:
-        file.write(soup.prettify())
-        logging.debug("保存成功")
+    years = ["2018", "2019"]
+    run(years)
