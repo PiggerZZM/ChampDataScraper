aebb7b876a58eb7b1b1a8afd4ff971ac5b55e2d0
PiggerZZM <442823793@qq.com>
2021-07-26 18:30:26 +0800

    modify get_all_match_urls.py use try except

--- scraper/get_all_match_urls.py
+++ scraper/get_all_match_urls.py
@@ -4,22 +4,39 @@ import requests
 import logging
 from bs4 import BeautifulSoup
 
-# 日志设置
-logging.basicConfig(level=logging.DEBUG,
-                    format="%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
-
 
 def get_schedule_detail_url(base_url: str, match: str, year: str, round: str) -> str:
+    """
+    拼接比赛赛程页的URL
+    :param base_url: "http://data.champdas.com"
+    :param match: 1或2，表示中超或中甲
+    :param year: 赛季
+    :param round: 轮数
+    :return: 比赛赛程页的URL
+    """
+
     return base_url + "/match/" + "scheduleDetail-" + match + "-" + year + "-" + round + ".html"
 
 
 def get_match_url(base_url: str, other: str) -> str:
+    """
+    拼接比赛详情页的URL
+    :param base_url: "http://data.champdas.com"
+    :param other: 包括比赛id号
+    :return: 比赛详情页的URL
+    """
+
     return base_url + other
 
 
-def get_all_match_urls() -> List[str]:
+def get_all_match_urls(years: List[str]) -> List[str]:
+    """
+    获取所有比赛详情页的URL
+    :param years: 赛季
+    :return: 所有比赛详情页的URL
+    """
+
     base_url = "http://data.champdas.com"
-    years = ["2018", "2019"]
     rounds = [str(i) for i in range(1, 31)]
     matches = ["1", "2"]
     match_urls = []
@@ -29,8 +46,10 @@ def get_all_match_urls() -> List[str]:
             for match in matches:
                 # 请求
                 schedule_detail_url = get_schedule_detail_url(base_url, match, year, round)
-                response = requests.get(schedule_detail_url)
-                if response.status_code == 200:
+                try:
+                    logging.info("get schedule detail url: " + schedule_detail_url)
+                    response = requests.get(schedule_detail_url)
+                    response.raise_for_status()
                     # 解析
                     html = response.text
                     soup = BeautifulSoup(html, "lxml")
@@ -39,11 +58,14 @@ def get_all_match_urls() -> List[str]:
                         other = match_note.a["href"]
                         match_url = get_match_url(base_url, other)
                         match_urls.append(match_url)
-                else:
-                    logging.warning(response.status_code)
-                    logging.warning(schedule_detail_url)
+                except requests.RequestException as e:
+                    logging.warning("get schedule detail url failed: " + schedule_detail_url)
+                    logging.warning(e)
+
     return match_urls
 
 
 if __name__ == '__main__':
-    match_urls = get_all_match_urls()
+    years = ["2018", "2019"]
+    match_urls = get_all_match_urls(years)
+    print(match_urls)
